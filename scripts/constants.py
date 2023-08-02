90855c928029ce0460b246a5b98306f27f838a92
filipemedeiross <filipemedeiros2013@gmail.com>
2023-08-02 11:32:00 -0300

    Added the files with the script constants

---
+++ scripts/constants.py
@@ -0,0 +1,6 @@
+cols = [
+    'institiuicoes_atuando_local', 'iniciados_outras_providencias', 'origem',
+    'plano_emergencia_acionado', 'informacao_responsavel', 'ocorrencia_oleo',
+    'informacao_geografica', 'tipos_fontes_informacoes', 'des_ocorrencia',
+    'uf', 'tipos_danos_identificados', 'periodo_ocorrencia', 'validado'
+]
